407246e0eba517fc3d463d446012240a4a119100
Rohan Varma <rvarm1@fb.com>
2021-03-25 16:12:05 -0700

    Provide a decorator to set/unset nccl blocking wait for tests
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/54740
    
    Adds a simple helper decorator to set/unset nccl blocking wait for
    tests. This will make it easier than having to manually set/unset the
    os.environ vars every time.
    ghstack-source-id: 124948684
    
    Differential Revision: [D27277222](https://our.internmc.facebook.com/intern/diff/D27277222/)

--- torch/testing/_internal/common_distributed.py
+++ torch/testing/_internal/common_distributed.py
@@ -104,6 +104,46 @@ def skip_if_lt_x_gpu(x):
 
     return decorator
 
+
+def with_nccl_blocking_wait(func):
+    """
+    Convenience decorator to set/unset NCCL_BLOCKING_WAIT flag. Note that use of
+    this decorator will override the setting of NCCL_ASYNC_ERROR_HANDLING for
+    the particular test. After the test, both NCCL_BLOCKING_WAIT and
+    NCCL_ASYNC_ERROR_HANDLING will be restored to their original values.
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # Save and unset NCCL_ASYNC_ERROR_HANDLING
+        try:
+            cached_nccl_async_error_handling = os.environ["NCCL_ASYNC_ERROR_HANDLING"]
+            del os.environ["NCCL_ASYNC_ERROR_HANDLING"]
+        except KeyError:
+            # NCCL_ASYNC_ERROR_HANDLING was unset
+            cached_nccl_async_error_handling = None
+
+        # Save val of NCCL_BLOCKING_WAIT and set it.
+        try:
+            cached_nccl_blocking_wait = os.environ["NCCL_BLOCKING_WAIT"]
+        except KeyError:
+            cached_nccl_blocking_wait = None
+        finally:
+            os.environ["NCCL_BLOCKING_WAIT"] = "1"
+
+        try:
+            ret = func(*args, **kwargs)
+            return ret
+        finally:
+            # restore old values.
+            if cached_nccl_async_error_handling is not None:
+                os.environ["NCCL_ASYNC_ERROR_HANDLING"] = cached_nccl_async_error_handling
+
+            if cached_nccl_blocking_wait is not None:
+                os.environ["NCCL_BLOCKING_WAIT"] = cached_nccl_blocking_wait
+
+    return wrapper
+
+
 def requires_gloo():
     return unittest.skipUnless(
         c10d.is_gloo_available(),
